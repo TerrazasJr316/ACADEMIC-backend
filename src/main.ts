74180574960b45878f8356daaa60996033101109
wuil <williamsmatiassan@gmail.com>
2026-01-11 20:33:53 -0600

    feat: implementar lógica de autenticación

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // --- ESTA ES LA PARTE CLAVE ---
+  // Permite que tu React (puerto 5173) pueda hacerle preguntas a NestJS
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  // ------------------------------
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Backend corriendo en: http://localhost:3000`);
 }
 bootstrap();
