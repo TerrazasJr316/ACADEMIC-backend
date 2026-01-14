899ca531fb438d9fa6fcc3eb570870356f64abc2
wuil2004 <williamsmatiassan@gmail.com>
2026-01-14 14:10:05 -0600

    modulos bd studen auth

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentProfile } from './entities/student-profile.entity'; // <--- Importar Entidad
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([StudentProfile]) // <--- ¡ESTO ES LO QUE FALTA!
+  ],
+  controllers: [],
+  providers: [],
+  exports: [TypeOrmModule]
+})
 export class StudentModule {}
