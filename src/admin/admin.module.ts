9d932e091af5f0415d3e2b8db8c8afaece58323c
LuisJoseCastro <luisjosecastrovidal@gmail.com>
2026-01-14 09:47:38 -0600

    Cambios

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminProfile } from './entities/admin-profile.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([AdminProfile])
+  ],
+  controllers: [],
+  providers: [],
+})
 export class AdminModule {}
