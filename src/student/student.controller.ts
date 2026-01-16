05c685c4b43d3a7c20504c27e6126f08775ce56c
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2026-01-16 04:16:42 -0600

    Integración de API de dashboard para el alumno conetcada a a la base de datos

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,20 +1,16 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { StudentService } from './student.service';
-// Asumiendo que usas el mismo sistema de Auth del líder
-// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'; 
 
 @Controller('student')
-// @UseGuards(JwtAuthGuard) // Descomentar cuando el líder pase el Auth
 export class StudentController {
     constructor(private readonly studentService: StudentService) { }
 
-    // Obtener el perfil completo para el Dashboard y Configuración
     @Get('profile/:userId')
     getProfile(@Param('userId') userId: string) {
         return this.studentService.getStudentProfile(userId);
     }
 
-    // Obtener resumen para el Dashboard (Notificaciones + Estadísticas)
+    // Ruta para getAlumnoDashboardSummary
     @Get('dashboard/summary/:userId')
     getDashboardSummary(@Param('userId') userId: string) {
         return this.studentService.getDashboardSummary(userId);
