5e43d04465a00a8030237ec61f1b28204ba0d25a
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2026-01-16 03:56:38 -0600

    Integración de API de notificaciones para alumno dentro de communications

--- src/communications/communications.module.ts
+++ src/communications/communications.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { InternalMessage } from './entities/internal-message.entity';
+import { CommunicationsService } from './communications.service';
+import { CommunicationsController } from './communications.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Notification, InternalMessage])
   ],
-  controllers: [],
-  providers: [],
+  controllers: [CommunicationsController],
+  providers: [CommunicationsService],
+  exports: [CommunicationsService]
 })
-export class CommunicationsModule {}
+export class CommunicationsModule { }
